51073ca6f00b1d54c3f6d5e658037d18237ba267
Marti Raudsepp <marti@juffo.org>
2020-02-16 14:14:37 +0200

    Minor cleanups

--- src/main.rs
+++ src/main.rs
@@ -80,7 +80,7 @@ fn parse_args<T: Into<OsString> + Clone>(args: impl IntoIterator<Item = T>) -> A
 }
 
 fn main_inner() -> Result<(), AnyErr> {
-    let args = parse_args(Box::new(env::args()));
+    let args = parse_args(env::args());
     logging::init_with_level(args.log_level);
 
     let mut vars: Vec<String> = Vec::new();
