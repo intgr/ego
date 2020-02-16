af411d73eaf8e8832bf8bf15d4f28590b7b6ba15
Marti Raudsepp <marti@juffo.org>
2020-02-16 08:04:48 +0200

    Add zsh completion support

---
+++ varia/ego-completion.zsh
@@ -0,0 +1,14 @@
+#compdef _ego ego
+# To make this autocompletion discoverable by zsh, run:
+#   sudo cp varia/ego-completion.zsh /usr/local/share/zsh/site-functions/_ego
+
+function _ego {
+    _arguments -C \
+        {-h,--help}'[Show help information]' \
+        {-u+,--user=}'[Specify a username]:user:_users' \
+        {-v,--verbose}'[Verbouse output]' \
+        '(-)1:command: _command_names -e' \
+        '*::arguments: _normal'
+}
+
+_ego
