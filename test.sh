2bc53c8f1a25636f00267b664a01a5f5aff61cb8
Marti Raudsepp <marti@juffo.org>
2021-02-06 19:22:54 +0200

    WIP

--- test.sh
+++ test.sh
@@ -8,5 +8,19 @@ IMG=ego-$DISTRO
 
 export DOCKER_BUILDKIT=1
 
+if [ "$DISTRO" = ubuntu ]; then
+  SYSTEMD=/bin/systemd
+else
+  SYSTEMD=/usr/lib/systemd/systemd
+fi
+
 docker build . ${2-} -f varia/Dockerfile.integration --build-arg=distro=$DISTRO -t $IMG
 docker run --rm $IMG sh -c 'id && ego --sudo id'
+docker run --rm -it \
+  -e container=docker \
+  --tmpfs /run \
+  --tmpfs /tmp \
+  -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
+  --cap-add SYS_ADMIN \
+  $IMG "$SYSTEMD" quiet systemd.firstboot=off \
+  systemd.setenv="CMD='id && mkdir -p /run/user/0 && XDG_RUNTIME_DIR=/run/user/0 ego --machinectl id'"
