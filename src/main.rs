intgr/ego

commit e1bb02d31200b32bf73fa9612a88c375df34d33a

Document xhost requirement, improve xhost error reporting (#76)
This should greatly clarify the situation reported in #66.

Refactored command execution functions into `util.rs`, created helper `run_command()`.

Output now if `xhost` is missing:
```
error: Error preparing X11: Failed to run xhost: No such file or directory (os error 2)
hint: Try installing package that contains command 'xhost'
```

src/main.rs
@@ -3,19 +3,17 @@ extern crate simple_error;
 
 use crate::cli::{parse_args, Method};
 use crate::errors::{print_error, AnyErr, ErrorWithHint};
-use crate::util::{have_command, sd_booted};
+use crate::util::{exec_command, have_command, run_command, sd_booted};
 use log::{debug, info, warn};
 use posix_acl::{PosixACL, Qualifier, ACL_EXECUTE, ACL_READ, ACL_RWX};
 use simple_error::SimpleError;
 use std::env::VarError;
 use std::fs::DirBuilder;
-use std::io::ErrorKind;
 use std::os::unix::fs::DirBuilderExt;
 use std::os::unix::fs::PermissionsExt;
-use std::os::unix::process::CommandExt;
 use std::path::{Path, PathBuf};
-use std::process::{exit, Command};
-use std::{env, fs, io};
+use std::process::exit;
+use std::{env, fs};
 use users::os::unix::UserExt;
 use users::{get_user_by_name, get_user_by_uid, uid_t, User};
 
@@ -195,18 +193,10 @@ fn prepare_x11(ctx: &EgoContext) -> Result<Vec<String>, AnyErr> {
         return Ok(vec![]);
     }
 
-    let grant = format!("+si:localuser:{}", ctx.target_user);
-    let ret = Command::new("xhost").arg(&grant).output()?;
-    if !ret.status.success() {
-        bail!(
-            "xhost returned {}:\n{}",
-            ret.status.code().unwrap_or(999),
-            String::from_utf8_lossy(&ret.stderr)
-        );
-    }
+    let grant = format!("-ak +si:localuser:{}", ctx.target_user);
+    run_command("xhost", &[grant])?;
     // TODO should also test /tmp/.X11-unix/X0 permissions?
 
-    debug!("X11 configured to allow {}", grant);
     Ok(vec![format!("DISPLAY={}", display.unwrap())])
 }
 
@@ -305,20 +295,6 @@ fn ensure_ego_rundir(ctx: &EgoContext) -> Result<PathBuf, AnyErr> {
     // Set ACL either way, because target user may be different in every run.
     add_file_acl(path.as_path(), ctx.target_uid, ACL_EXECUTE)?;
     Ok(path)
-}
-
-fn exec_command(program: &str, args: &[String]) -> Result<(), ErrorWithHint> {
-    // If this call returns at all, it was an error
-    let err: io::Error = Command::new(program).args(args).exec();
-
-    Err(ErrorWithHint::new(
-        format!("Failed executing {}: {}", program, err),
-        if err.kind() == ErrorKind::NotFound {
-            format!("Try installing package that contains command '{}'", program)
-        } else {
-            format!("Complete command: {} {}", program, shell_words::join(args))
-        },
-    ))
 }
 
 /// Detect which method should be used

src/util.rs
@@ -1,5 +1,10 @@
-use std::env;
+use crate::ErrorWithHint;
+use log::debug;
+use std::io::ErrorKind;
+use std::os::unix::prelude::CommandExt;
 use std::path::Path;
+use std::process::{Command, Output};
+use std::{env, io};
 
 /// Detect if system was booted with systemd init system. Same logic as sd_booted() in libsystemd.
 /// https://www.freedesktop.org/software/systemd/man/sd_booted.html
@@ -13,4 +18,46 @@ pub fn have_command<P: AsRef<Path>>(exe_name: P) -> bool {
     env::var_os("PATH").map_or(false, |paths| {
         env::split_paths(&paths).any(|dir| dir.join(&exe_name).is_file())
     })
+}
+
+fn report_command_error(err: io::Error, program: &str, args: &[String]) -> ErrorWithHint {
+    ErrorWithHint::new(
+        format!("Failed to run {}: {}", program, err),
+        if err.kind() == ErrorKind::NotFound {
+            format!("Try installing package that contains command '{}'", program)
+        } else {
+            format!("Complete command: {} {}", program, shell_words::join(args))
+        },
+    )
+}
+
+/// Exec command (ending the current process) or return error.
+pub fn exec_command(program: &str, args: &[String]) -> Result<(), ErrorWithHint> {
+    debug!("Executing: {} {}", program, shell_words::join(args));
+    // If this call returns at all, it was an error
+    let err: io::Error = Command::new(program).args(args).exec();
+
+    Err(report_command_error(err, program, args))
+}
+
+/// Run command as subprocess. Return output if status was 0, otherwise return as error.
+pub fn run_command(program: &str, args: &[String]) -> Result<Output, ErrorWithHint> {
+    debug!("Running: {} {}", program, shell_words::join(args));
+    let ret = Command::new(program)
+        .args(args)
+        .output()
+        .map_err(|err| report_command_error(err, program, args))?;
+
+    if !ret.status.success() {
+        return Err(ErrorWithHint::new(
+            format!(
+                "{} returned {}:\n{}",
+                program,
+                ret.status.code().unwrap_or(999),
+                String::from_utf8_lossy(&ret.stderr).trim()
+            ),
+            format!("Complete command: {} {}", program, shell_words::join(args)),
+        ));
+    }
+    Ok(ret)
 }

commit 25e9cfb5fa5acd9ce8abaa87bd2c9f4eab4a6d1c

Prepare for 1.1.5 release (fix regression in 1.1.4)
Fixes glaring regression introduced in #76.

Cargo.toml
@@ -1,6 +1,6 @@
 [package]
 name = "ego"
-version = "1.1.4"
+version = "1.1.5"
 edition = "2018"
 
 # Metadata

src/main.rs
@@ -193,7 +193,7 @@ fn prepare_x11(ctx: &EgoContext) -> Result<Vec<String>, AnyErr> {
         return Ok(vec![]);
     }
 
-    let grant = format!("-ak +si:localuser:{}", ctx.target_user);
+    let grant = format!("+si:localuser:{}", ctx.target_user);
     run_command("xhost", &[grant])?;
     // TODO should also test /tmp/.X11-unix/X0 permissions?
 