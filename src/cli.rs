51470ac6159a94e38e4cc88f93d4c914fa97c1cd
Marti Raudsepp <marti@juffo.org>
2022-09-29 00:30:23 +0300

    Migrate to clap v4.0 (#101)
    Changelog: https://github.com/clap-rs/clap/blob/master/CHANGELOG.md
    
    Blog: https://epage.github.io/blog/2022/09/clap4/
    
    Closes #99, closes #100

--- src/cli.rs
+++ src/cli.rs
@@ -1,4 +1,4 @@
-use clap::{Arg, ArgGroup, Command, ValueHint};
+use clap::{Arg, ArgAction, ArgGroup, Command, ValueHint};
 use log::Level;
 use std::ffi::OsString;
 
@@ -17,46 +17,49 @@ pub struct Args {
     pub method: Option<Method>,
 }
 
-pub fn build_cli() -> Command<'static> {
+pub fn build_cli() -> Command {
     Command::new("Alter Ego: run desktop applications under a different local user")
-        .trailing_var_arg(true)
         .disable_version_flag(true)
         .arg(
             Arg::new("user")
                 .short('u')
                 .long("user")
                 .value_name("USER")
+                .default_value("ego")
                 .help("Specify a username (default: ego)")
-                .takes_value(true)
                 .value_hint(ValueHint::Username),
         )
         .arg(
             Arg::new("sudo")
                 .long("sudo")
+                .action(ArgAction::SetTrue)
                 .help("Use 'sudo' to change user"),
         )
         .arg(
             Arg::new("machinectl")
                 .long("machinectl")
+                .action(ArgAction::SetTrue)
                 .help("Use 'machinectl' to change user (default, if available)"),
         )
         .arg(
             Arg::new("machinectl-bare")
                 .long("machinectl-bare")
+                .action(ArgAction::SetTrue)
                 .help("Use 'machinectl' but skip xdg-desktop-portal setup"),
         )
         .group(ArgGroup::new("method").args(&["sudo", "machinectl", "machinectl-bare"]))
         .arg(
             Arg::new("command")
                 .help("Command name and arguments to run (default: user shell)")
-                .multiple_values(true)
+                .num_args(1..)
+                .trailing_var_arg(true)
                 .value_hint(ValueHint::CommandWithArguments),
         )
         .arg(
             Arg::new("verbose")
                 .short('v')
                 .long("verbose")
-                .multiple_occurrences(true)
+                .action(ArgAction::Count)
                 .help("Verbose output. Use multiple times for more output."),
         )
 }
@@ -65,23 +68,23 @@ pub fn parse_args<T: Into<OsString> + Clone>(args: impl IntoIterator<Item = T>) 
     let matches = build_cli().get_matches_from(args);
 
     Args {
-        user: matches.value_of("user").unwrap_or("ego").to_string(),
+        user: matches.get_one::<String>("user").unwrap().clone(),
         command: matches
-            .values_of("command")
+            .get_many("command")
             .unwrap_or_default()
-            .map(|v| v.to_string())
+            .cloned()
             .collect(),
-        log_level: match matches.occurrences_of("verbose") {
+        log_level: match matches.get_count("verbose") {
             0 => Level::Warn,
             1 => Level::Info,
             2 => Level::Debug,
             _ => Level::Trace,
         },
-        method: if matches.is_present("machinectl") {
+        method: if matches.get_flag("machinectl") {
             Some(Method::Machinectl)
-        } else if matches.is_present("machinectl-bare") {
+        } else if matches.get_flag("machinectl-bare") {
             Some(Method::MachinectlBare)
-        } else if matches.is_present("sudo") {
+        } else if matches.get_flag("sudo") {
             Some(Method::Sudo)
         } else {
             None
--- varia/ego-completion.fish
+++ varia/ego-completion.fish
@@ -1,6 +1,6 @@
 complete -c ego -s u -l user -d 'Specify a username (default: ego)' -r -f -a "(__fish_complete_users)"
-complete -c ego -s h -l help -d 'Print help information'
 complete -c ego -l sudo -d 'Use \'sudo\' to change user'
 complete -c ego -l machinectl -d 'Use \'machinectl\' to change user (default, if available)'
 complete -c ego -l machinectl-bare -d 'Use \'machinectl\' but skip xdg-desktop-portal setup'
 complete -c ego -s v -l verbose -d 'Verbose output. Use multiple times for more output.'
+complete -c ego -s h -l help -d 'Print help information'
