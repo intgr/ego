d04c57f48f7ea80cfa7a5f406e4c8f9b20cdceef
Marti Raudsepp <marti@juffo.org>
2022-10-18 23:14:17 +0300

    Update dependencies

--- varia/ego-completion.bash
+++ varia/ego-completion.bash
@@ -8,8 +8,8 @@ _ego() {
 
     for i in ${COMP_WORDS[@]}
     do
-        case "${i}" in
-            "$1")
+        case "${cmd},${i}" in
+            ",$1")
                 cmd="ego"
                 ;;
             *)
